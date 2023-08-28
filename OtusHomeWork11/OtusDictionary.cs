b0c2f3a729575c95ca3f882ee0f47017ef061b71
volodie <119340399+volodie@users.noreply.github.com>
2023-08-28 14:38:42 +0300

    OtusHomeWork11

---
+++ OtusHomeWork11/OtusDictionary.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtusHomeWork11
+{
+    internal class OtusDictionary
+    {
+        int size = 32;
+        OtusKeyValuePair[] values;
+
+        public OtusDictionary()
+        {
+            values = new OtusKeyValuePair[size];
+        }
+
+        public string this[int key]
+        {
+            get { return Get(key); }
+            set { Add(key, value); }
+        }
+
+        public void Add(int key, string value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+
+            var hash = GetHash(key);
+
+            while (values[hash] != null)
+            {
+                if (values[hash].Key == key) throw new ArgumentException("Ключ уже существует.");
+
+                values = IncreaseArraySize();
+            }
+
+            values[hash] = new OtusKeyValuePair() { Key = key, Value = value };
+        }
+
+        public string Get(int key) => values[GetHash(key)] != null
+            ? values[GetHash(key)].Value
+            : throw new ArgumentOutOfRangeException(nameof(key));
+
+        OtusKeyValuePair[] IncreaseArraySize()
+        {
+            size *= 2;
+
+            var newValues = new OtusKeyValuePair[size];
+
+            foreach (var pair in values)
+            {
+                if (pair == null) continue;
+
+                var hash = GetHash(pair.Key);
+
+                if (newValues[hash] != null) return IncreaseArraySize();
+
+                newValues[hash] = pair;
+            }
+
+            return newValues;
+        }
+
+        int GetHash(int key) => Math.Abs(key % size);
+
+        class OtusKeyValuePair
+        {
+            public int Key { get; set; }
+            public string Value { get; set; } = null!;
+        }
+    }
+}
---
+++ OtusHomeWork11/Program.cs
@@ -0,0 +1,20 @@
+﻿using OtusHomeWork11;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var dict = new OtusDictionary();
+
+        dict.Add(3, "3");
+        dict.Add(10, "10");
+        //dict.Add(10, "10"); //ключ уже занят
+        dict.Add(132, "132");
+        dict.Add(70, "70");
+        dict.Add(54, "54");
+        dict.Add(0, "0");
+        dict.Add(1, "1");
+        var s = dict.Get(132);
+        Console.WriteLine(s);
+    }
+}
